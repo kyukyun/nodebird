ae9ce350eff9a627170d7b0fac436ca3eae2b40f
kyukyun <163502143+kyukyun@users.noreply.github.com>
2024-05-25 23:22:17 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+const path = require('path');
+const session = require('express-session');
+const nunjucks = require('nunjucks');
+const dontev = require('dotenv');
+
+dontev.config();
+const pageRouter = require('./routes/page');
+
+const app = express();
+app.set('port', process.env.PORT || 8001);
+app.set('view engine', 'html');
+nunjucks.configure('views', {
+    express: app,
+    watch: true,
+});
+
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
+app.use(session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    secret: {
+        httpOnly: true,
+        secure: false,
+    }
+}));
+
+app.use('/', pageRouter);
+
+app.use((req, res, next) => {
+    const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+    error.status = 404;
+    next(error);
+});
+
+app.use((err, req, res, next) => {
+    res.local.message = err.message;
+    res.local.error = process.env.NODE_ENV !== 'production' ? err : {};
+    res.status(ree.status || 500);
+    res.render('error');
+});
+
+app.listen(app.get('port'), () => {
+    console.log(app.get('port'), '번 포트에서 대기중');
+});
